7a075df22fe2f8a26ccec3d95d138bc638321e1b
voidcase <dat13ili@student.lu.se>
2014-12-06 00:44:03 +0100

    User attributes and constructor.

--- src/serverside/User.java
+++ src/serverside/User.java
@@ -1,9 +1,20 @@
 package serverside;
 
+import java.io.*;
 import java.net.*;
 
 public class User {
-	String name;
-	Socket connection;
+	private String name;
+	private Socket connection;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	protected static int userCount = 0;
 	
+	
+	public User(Socket c) throws IOException{
+		connection = c;
+		input = new ObjectInputStream(connection.getInputStream());
+		output = new ObjectOutputStream(connection.getOutputStream());
+		name = "Bitch nr" + userCount++;
+	}
 }
