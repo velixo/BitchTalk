60c31264cb5c51a9da4efd2e7b995ddf8fd336c9
Vilhelm <v.lundqvist94@gmail.com>
2014-12-06 22:19:46 +0100

    added some command strings that as ideas for commands

--- src/command/CommandFactory.java
+++ src/command/CommandFactory.java
@@ -1,6 +1,11 @@
 package command;
 
 public class CommandFactory {
+	public static String CONNECT= "connect";
+	public static String NAMECHANGE= "namechange";
+	
+	public static String WOOLOOLOO= "woolooloo";
+	public static String GAFFELTRUCK = "gaffeltruck";
 
 	public CommandFactory() {
 		
