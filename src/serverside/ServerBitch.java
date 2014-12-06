ac7f6d0b81744ba82d51196e00c8c3b1f6ef6a0c
voidcase <dat13ili@student.lu.se>
2014-12-06 01:19:33 +0100

    imp. waitForConnection()

--- src/serverside/ServerBitch.java
+++ src/serverside/ServerBitch.java
@@ -1,7 +1,26 @@
 package serverside;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ServerBitch {
+	
+	ServerSocket gatekeeper;
+	ServerGui gui;
+	ArrayList<User> userList = new ArrayList<User>();
+	
 	private void waitForConnection(){
-		
+		Socket s;
+		try {
+			s = gatekeeper.accept();
+			userList.add(new User(s));
+			gui.showMessage("some bitch has joined.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			gui.showMessage("någon är för dålig för att connecta.");
+			e.printStackTrace();
+		}
 	}
 }
