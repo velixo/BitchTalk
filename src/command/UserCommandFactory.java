9d6609ed979ddc7524cdbb19286d4d073d8f3f6e
Vilhelm <v.lundqvist94@gmail.com>
2014-12-06 22:51:48 +0100

    Cleaned up UserCommandFactory

--- src/command/UserCommandFactory.java
+++ src/command/UserCommandFactory.java
@@ -1,23 +1,24 @@
 package command;
 
 import serverside.Server;
+import serverside.User;
 
 import command.clientside.SetName;
-import command.serverside.Gaffeltruck;
 
 public class UserCommandFactory {
 	public final static String SETNAME= "setname";
 	private Server server;
+	private User u;
 
-	public UserCommandFactory(Server s) {
-		server = s;
+	public UserCommandFactory(User u) {
+		this.u = u;
 	}
 	
 	public Command build(String input) {
 		switch (input) {
 
 		case SETNAME:
-			return new SetName();
+			return new SetName(u, input);
 		
 		default:
 			break;
