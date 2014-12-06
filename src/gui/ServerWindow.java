77d34c6d9b33bf5198adbeacc421ff91bdda91bd
Vilhelm <v.lundqvist94@gmail.com>
2014-12-07 00:39:09 +0100

    Added usersInChat area to Server window

--- src/gui/ServerWindow.java
+++ src/gui/ServerWindow.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -10,12 +11,18 @@ import serverside.ServerGui;
 
 public class ServerWindow extends JFrame implements ServerGui {
 	private JTextArea chatWindow;
+	private JTextArea usersInConvoWindow;
 
 	public ServerWindow() {
 		super("BitchTalk Server: ");
 		chatWindow = new JTextArea();
 		chatWindow.setEditable(false);
 		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+		
+		usersInConvoWindow = new JTextArea();
+		usersInConvoWindow.setEditable(false);
+		usersInConvoWindow.append("Users currently in this chat: \n");
+		add(new JScrollPane(usersInConvoWindow), BorderLayout.EAST);
 		
 		setSize(450,550);
 		setVisible(true);
@@ -25,6 +32,13 @@ public class ServerWindow extends JFrame implements ServerGui {
 	@Override
 	public void showMessage(String m) {
 		chatWindow.append(m + "\n");
+	}
+	
+	public void updateUsersWindow(List<String> users) {
+		usersInConvoWindow.setText("Users currently in this chat:\n");
+		for (String u : users) {
+			usersInConvoWindow.append(u + "\n");
+		}
 	}
 
 }
