195fc911e1332507d18fd386473c5240cce96bab
voidcase <dat13ili@student.lu.se>
2014-12-06 01:51:55 +0100

    refactored server name

--- src/serverside/Server.java
+++ src/serverside/Server.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class ServerBitch {
+public class Server {
 	
 	ServerSocket gatekeeper;
 	ServerGui gui;
