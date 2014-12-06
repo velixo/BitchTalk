919e384dfaa8e867654293ae60fa0c833b16f0b4
Vilhelm <v.lundqvist94@gmail.com>
2014-12-06 01:38:08 +0100

    Removed uneccessary classes

--- src/gui/DiscussionWindow.java
+++
@@ -1,6 +0,0 @@
-package gui;
-
-/** Class that shows a debate with all the arguments, questions, etc. */
-public class DiscussionWindow {
-
-}
--- src/gui/StartWindow.java
+++
@@ -1,8 +0,0 @@
-package gui;
-
-/** The window that shows at start.
- * TODO Suggestions:
- * 	Shows a list of discussions/debates. Can be filtered with - Popular, New, Recent */
-public class StartWindow {
-
-}
