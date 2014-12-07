2070c33785d8b17e146c6939e7892c3cc959587a
Vilhelm <v.lundqvist94@gmail.com>
2014-12-07 16:15:27 +0100

    WOOP SOUND KINDA WORKS

--- src/gui/ClientWindow.java
+++ src/gui/ClientWindow.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class ClientWindow extends JFrame implements ClientGui {
 	private JTextArea usersInConvoWindow;
 	
 	private Clip clip;
-	private final String notificationSoundName = "messageNotificationSound.wav";
+	private final String notificationSoundName = "notificationSound_16bit.wav";
 	private boolean notificationSoundLoaded = false;
 	
 	public ClientWindow() {
@@ -59,9 +60,9 @@ public class ClientWindow extends JFrame implements ClientGui {
 	@Override
 	public void showMessage(String m) {
 		chatWindow.append(m + "\n");
-//		playNotificationSound();	//TODO remove, this is purely for testing
+		playNotificationSound();	//TODO remove, this is purely for testing
 		if (!isActive() || !isFocused()) {	//not sure which one to use or what the difference is
-			playNotificationSound();
+//			playNotificationSound();
 		}
 	}
 	
@@ -75,7 +76,10 @@ public class ClientWindow extends JFrame implements ClientGui {
 		try {
 			clip = AudioSystem.getClip();
 			//TODO fixa pathen till notifification sound
-			AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("res/" + notificationSoundName));
+			File file = new File("res/" + notificationSoundName);
+			System.out.println(file.toString());
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(file);
+//			AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("/res/" + notificationSoundName));
 			clip.open(inputStream);
 			notificationSoundLoaded = true;
 		} catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
