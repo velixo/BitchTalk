e267abfe25b1739393b350ab28fe78c8360e0104
Vilhelm <v.lundqvist94@gmail.com>
2015-02-10 21:39:15 +0100

    Started working on an implementation of message, added stub unit test to accompany it

--- src/shared/Message.java
+++ src/shared/Message.java
@@ -3,12 +3,26 @@ package shared;
 import java.io.Serializable;
 
 public class Message<E extends Serializable> implements Serializable {
+	public static final String USERLIST = "userlist";
+	public static final String COMMAND = "command";
+	public static final String TEXT = "text";
+	
 	private static final long serialVersionUID = 732925911515696139L;
+	private String messageType;
 	private String sender;
 	private E content;
 	
-	public Message(String sender, E content) {
+	public Message(String sender, String messageType, E content) {
 		this.sender = sender;
+		this.messageType = messageType;
 		this.content = content;
+	}
+	
+	public String getType() {
+		return messageType;
+	}
+	
+	public E getContent() {
+		return content;
 	}
 }
---
+++ src/test/TestMessage.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import shared.Message;
+
+public class TestMessage {
+
+	@Test
+	public void testTextMessage() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCommand() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testUserList() {
+		fail("Not yet implemented");
+	}
+
+}
