47794149a5de957fea177d09c5fcd1b689fd18d6
Vilhelm <v.lundqvist94@gmail.com>
2015-02-10 22:35:36 +0100

    Implemented Message

--- src/shared/Message.java
+++ src/shared/Message.java
@@ -12,6 +12,13 @@ public class Message<E extends Serializable> implements Serializable {
 	private String sender;
 	private E content;
 	
+	/**
+	 * ASDASDASDASD
+	 * 
+	 * @param sender
+	 * @param messageType
+	 * @param content
+	 */
 	public Message(String sender, String messageType, E content) {
 		this.sender = sender;
 		this.messageType = messageType;
@@ -24,5 +31,17 @@ public class Message<E extends Serializable> implements Serializable {
 	
 	public E getContent() {
 		return content;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Message<?>) {
+			Message<E> m = (Message<E>) o;
+			return content.equals(m.content)
+				&& sender.equals(m.sender)
+				&& messageType.equals(m.messageType);
+		}
+		return false;
 	}
 }
